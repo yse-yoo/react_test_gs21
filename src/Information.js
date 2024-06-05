8b870ca4d6a97ae8c7b88b7c7a7a723307cf90f9
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-06-05 13:12:16 +0900

    update

--- src/Information.js
+++ src/Information.js
@@ -2,9 +2,14 @@ import React, { useState } from 'react'
 
 function Information() {
     const [isShow, setIsShow] = useState(false);
+;
+    const showHandler = () => {
+        setIsShow(!isShow)
+    }
 
     return (
         <div>
+            <button onClick={showHandler}>{isShow ? "Hide" : "Show"}</button>
             {isShow &&
             <ul>
                 <li>Information1</li>
