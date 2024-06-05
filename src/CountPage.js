a19ae511a707edb1c252191ebef70b9681645777
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-06-05 11:27:05 +0900

    update

--- src/CountPage.js
+++ src/CountPage.js
@@ -3,10 +3,15 @@ import React, { useState } from 'react'
 function CountPage() {
     const [count, setCount] = useState(0);
     
+    const countUpHandler = () => {
+        console.log("Click!")
+    }
+
     return (
         <div>
             <h2>CountPage</h2>
             <p>{count}</p>
+            <button onClick={countUpHandler}>Count Up</button>
         </div>
     )
 }
