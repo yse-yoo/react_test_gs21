4baf644c236a081aa201225857ee5a80a17735c5
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-06-05 13:20:52 +0900

    update

--- src/Information.js
+++ src/Information.js
@@ -2,7 +2,12 @@ import React, { useState } from 'react'
 
 function Information() {
     const [isShow, setIsShow] = useState(false);
-;
+
+    var informations = [
+        { id: 1, updated_at: "2024/06/05", title: "DMMビットコイン流出" },
+        { id: 2, updated_at: "2024/06/04", title: "乳酸菌飲料ブーム" },
+        { id: 3, updated_at: "2024/06/03", title: "「空飛ぶ基地局」26年に商用化" },
+    ];
     const showHandler = () => {
         setIsShow(!isShow)
     }
@@ -11,11 +16,16 @@ function Information() {
         <div>
             <button onClick={showHandler}>{isShow ? "Hide" : "Show"}</button>
             {isShow &&
-            <ul>
-                <li>Information1</li>
-                <li>Information2</li>
-                <li>Information3</li>
-            </ul>
+                <ul>
+                    {
+                    informations.map((info) => (
+                        <li key={info.id}>
+                            {info.title}
+                            ({info.updated_at})
+                        </li>
+                    )
+                    )}
+                </ul>
             }
         </div>
     )
